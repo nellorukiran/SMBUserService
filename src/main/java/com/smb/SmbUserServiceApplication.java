78d081292f64e9f09e5b601842f16fca0868ca79
nellorukiran <nellorukiran@gmail.com>
2023-03-17 16:12:05 +0530

    First Commit

---
+++ src/main/java/com/smb/SmbUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmbUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmbUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smb/SmbUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmbUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
